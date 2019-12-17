b0186bcd40a1371e11bc33ffcf10f15173edfc4d
Calvin <cvan@capgroup.com>
2019-12-17 11:45:22 -0800

    added authentication and authorization guards to "createRoster" query

--- main.ts
+++ main.ts
@@ -2,13 +2,14 @@
 import cors from 'cors'
 import express from 'express'
 import { ApolloServer, gql } from 'apollo-server'
-import { UserIdentity, deriveUserFromToken } from './identity/auth'
+import { UserIdentity, deriveUserFromToken, isAuthenticated, requringRole } from './identity/auth'
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String,
-    me: User
+    me: User,
+    createRoster: [User]
   }
 
   type User {
@@ -20,12 +21,31 @@ const typeDefs = gql`
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
+    // Public: does not require an authenticated user to query
     hello: () => 'Hello from your Apollo GraphQL Typescript Server!',
-    me: (root: any, args: any, context: any) : UserIdentity => {
-      const identity = context.userIdentity
+    
+    // Protected: requires an authenticated user to query
+    me: isAuthenticated((_parent: any, _args: any, context: any) : UserIdentity => {
+      const identity = context.currentUser
       console.log({ identity })
-      return context.userIdentity
-    }
+      return context.currentUser // Note: this exposes the current user as `context.currentUser`
+    }),
+
+    // Protected witxh role requirement: requires an authenticated user eligible correct role to query
+    createRoster: 
+      isAuthenticated(
+        requringRole('coach')(
+          (root: any, args: any, context: any) : UserIdentity[] => {
+              return [
+                <UserIdentity>{_id: 'lb23', name: 'Lebron James'},
+                <UserIdentity>{_id: 'kl02', name: 'Kawhi Leonard'},
+                <UserIdentity>{_id: 'kl02', name: 'Kobe Bryant'},
+                <UserIdentity>{_id: 'kl02', name: 'Shaquille Oneal'},
+                <UserIdentity>{_id: 'kl02', name: 'James Worthy'},
+              ]
+          }
+        )
+      )  
   },
   User: {
     id: (user: any) => user._id,
@@ -39,8 +59,9 @@ const server = new ApolloServer({
     const accessToken = req.headers.authorization
     const userIdentity = await deriveUserFromToken(accessToken)
     console.log({userIdentity})
+
     return {
-      userIdentity
+      currentUser: Object.freeze(userIdentity)
     }
   },
   resolvers
